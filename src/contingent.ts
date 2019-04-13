388f47ee202d0cf0ee7cec350b134c09a6184d6f
James Abney <james.abney@gmail.com>
2019-04-13 15:38:14 -0700

    Add contingent.ts

---
+++ src/contingent.ts
@@ -0,0 +1,88 @@
+import cryptoLib from './crypto-lib'
+import { swap } from './utils'
+
+const crypto = cryptoLib()
+
+/**
+ * Return true or false at random.
+ */
+export function randomBit(): boolean {
+  return crypto.randomBit()
+}
+
+/**
+ * Return a random byte in the range [0, 255].
+ */
+export function randomByte(): number {
+  return crypto.randomByte()
+}
+
+/**
+ * Return a random 32 bit signed integer.
+ */
+export function randomInt(): number {
+  return crypto.randomInt()
+}
+
+/**
+ * Return a random 32 bit unsigned integer.
+ */
+export function randomUInt(): number {
+  return crypto.randomUint()
+}
+
+/**
+ * Return a random float in the range [0, 1).
+ */
+export function randomFloat(): number {
+  return crypto.randomUint() / 2**32
+}
+
+/**
+ * Return a positive random number in the range [min, max).
+ *
+ * @param value the minimum value
+ * @param max the maximum value, inclusive
+ */
+export function randomIn(min: number, max: number): number {
+  if (min < 0 || max < 0) {
+    throw new Error('min and max should be >= 0')
+  }
+  if (max < min) {
+    throw new Error('max should be >= min')
+  }
+  return min + Math.floor((max - min) * randomFloat())
+}
+
+/**
+ * Return a random value from the given list.
+ *
+ * @param list the list from which to select a value.
+ */
+export function randomOf<T>(list: T[]): T {
+  return list[randomIn(0, list.length)]
+}
+
+/**
+ * Shuffle elements of an array in place.
+ */
+export function shuffle<T>(list: T[]): T[] {
+  for (let i = 0; i < list.length; i++) {
+    const elem = randomIn(i, list.length)
+    swap(list, i, elem)
+  }
+  return list
+}
+
+/**
+ * Pick _n_ random values from a list.
+ */
+export function pick<T>(list: T[], num: number): T[] {
+  if (num < 0 || num > list.length) {
+    throw new Error(`num should be in the range [0, ${list.length}]`)
+  }
+
+  const copy = list.slice()
+  shuffle(copy)
+  return copy.slice(-num)
+}
