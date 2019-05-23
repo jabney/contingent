a32c4556db4869dff1eeafb04346b52ec4b9b9a3
James Abney <james.abney@gmail.com>
2019-05-23 14:12:56 -0700

    Update config for multiple outputs

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -1,12 +1,17 @@
 // https://webpack.js.org/guides/author-libraries/
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './dist/index.js',
+  // entry: './dist/index.js',
+  entry: {
+    'contingent': './dist/index.js',
+    'contingent.min': './dist/index.js',
+  },
   mode: 'production',
   output: {
     path: path.resolve(__dirname, '.'),
-    filename: 'contingent.js',
+    // filename: 'contingent.js',
     library: 'contingent',
     libraryExport: 'contingent',
     libraryTarget: 'umd'
@@ -15,7 +20,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -23,6 +28,14 @@ module.exports = {
           }
         }
       }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        include: /\.min.js$/
+      })
     ]
   }
   // stats: { warnings: false }
